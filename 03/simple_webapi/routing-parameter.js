afa32a8037b11b64231b00bc85f786f4f237ed95
GoldenDoG27 <papersuper@gmail.com>
2020-08-21 15:59:47 +0700

    routing-two-parameter

--- 03/simple_webapi/routing-parameter.js
+++ 03/simple_webapi/routing-parameter.js
@@ -1,45 +1,71 @@
-//step 1 create object express
+// step 1 create object express
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 8000;
+// step 2 routing
 
-// IT Studens
+// IT students
 const itStudents = [
-    {id:61, name:'Suchada'},
-    {id:62, name:'Narawit'}
+    {id:61,name:'Suchada IT'},
+    {id:62,name:'Narawit IT'}
 ];
-
-//CE Students
+// CE students
 const ceStudents = [
-    {id:71, name:'Chonticha'},
-    {id:72, name:'Kiattiphom'}
+    {id:71,name:'Chonticha CE'},
+    {id:72,name:'Kiattiphom CE'}
 ];
 
 //major
-const allmajors = {
-    'it': itStudents,
-    'ce': ceStudents
+const allMajors = {
+    'it':itStudents,
+    'ce':ceStudents
 };
 
-//route1
+//route
 app.get('/api/:major',(req,res,next)=>{
     const major = req.params.major.toLowerCase();
-    if (major=='it'){
-        const jsonString = JSON.stringify(itStudents);
+    if(major == 'it'){
+        const jsonString = JSON.stringify(itStudents)
         req.myobj = jsonString;
         next();
-    }else if(major =='ce'){
-        const jsonString = JSON.stringify(ceStudents);
+    }else if(major == 'ce'){
+        const jsonString = JSON.stringify(ceStudents)
         req.myobj = jsonString;
         next();
     }else{
-        res.send('Major not found!!!');
+        res.send('Wrong parameter');
     }
 },(req,res)=>{
-    req.myobj += 'End of Data';
-    res.send(req.myobj);
+    req.myobj += ' End of Data';
+    res,send(req.myobj);
 });
+// route2
+app.get('/api/:id/:major',(req,res)=>{
+    const id = req.params.id;
+    const major = req.params.major.toLowerCase();
 
+    if(allMajors[major]){
+        if(major == 'it'){
+            let stuData = itStudents.find(obj => obj.id === parseInt(id));
+            if (typeof stuData === 'undefined'){
+                res.send('This student is no in major IT');
+            }else{
+                res.send(stuData);
+            }
+        }else{
+            let stuData = ceStudents.find(obj => obj.id === parseInt(id));
+            if(typeof stuData === 'undefined'){
+                res.send('This student is no in major CE');
+            }else{
+                res.send(stuData);
+            }
+        }
+    }else{
+        res.send('Major not Found!!!');
+    }
+});
+//step 3 listen
 app.listen(port,'127.0.0.1',()=>{
-    console.log(`Listening to request on port ${port}`);
+    console.log(`Listening on port ${port}`)
 });
